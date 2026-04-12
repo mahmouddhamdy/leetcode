3b7276fcfa5e36054ac0b0d9de8af979c121710d
MahmoudHamdy <mahmouddhamdy7@gmail.com>
2026-04-12 13:30:19 +0200

    704. Binary Search [Iterative]

--- #704.ts
+++ #704.ts
@@ -1,15 +1,17 @@
-function search(nums: number[], target: number, offset = 0): number {
-  const midIndex = Math.floor((nums.length - 1) / 2);
-
-  if (target === nums[midIndex]) {
-    return offset + midIndex;
-  } else if (target > nums[midIndex]) {
-    const newNums = nums.slice(midIndex + 1);
-    return search(newNums, target, offset + midIndex + 1);
-  } else if (target < nums[midIndex]) {
-    const newNums = nums.slice(0, midIndex);
-    return search(newNums, target, offset);
+function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (target === nums[mid]) {
+      return mid;
+    } else if (target > nums[mid]) {
+      left = mid + 1;
+    } else if (target < nums[mid]) {
+      right = mid - 1;
+    }
   }
-
   return -1;
 }
+
+search([-1, 0, 3, 5, 9, 12], 9);
